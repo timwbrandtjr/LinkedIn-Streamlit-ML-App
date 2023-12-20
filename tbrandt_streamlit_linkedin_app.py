062a3920c1907d2e4a8db0dc89c369e8e23f2f37
Timothy Brandt <153303831+timwbrandtjr@users.noreply.github.com>
2023-12-20 16:45:06 -0500

    Update tbrandt_streamlit_linkedin_app.py

--- tbrandt_streamlit_linkedin_app.py
+++ tbrandt_streamlit_linkedin_app.py
@@ -75,7 +75,8 @@ def predict_probability(features, scaler, model):
     return probability
 
 def main():
-    st.title("LinkedIn User Prediction App")
+     st.set_page_config(page_title="LinkedIn User Prediction App", page_icon=":bar_chart:", layout="wide")
+    st.markdown("<h1 style='color: #0a66c2;'>LinkedIn User Prediction App</h1>", unsafe_allow_html=True)
 
     st.markdown("""
             This app predicts the probability that a user is a LinkedIn member based on their 

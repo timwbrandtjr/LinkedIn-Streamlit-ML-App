0cb6a61188e040219503a0df412561630c84bd58
Timothy Brandt <153303831+timwbrandtjr@users.noreply.github.com>
2023-12-20 16:40:26 -0500

    Update tbrandt_streamlit_linkedin_app.py

--- tbrandt_streamlit_linkedin_app.py
+++ tbrandt_streamlit_linkedin_app.py
@@ -112,9 +112,9 @@ def main():
     st.write(f"Probability of being a LinkedIn user: {probability_pct:.2f}%")
 
     if probability_pct >= 50:
-    prediction = "LinkedIn User"
+        prediction = "LinkedIn User"
     else:
-    prediction = "Non-LinkedIn User"
+        prediction = "Non-LinkedIn User"
 
     st.write(f"Prediction: {prediction}")
 

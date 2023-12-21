4b6c44f7719ca6da05755f5abbefcb7b3e4138f4
Timothy Brandt <153303831+timwbrandtjr@users.noreply.github.com>
2023-12-21 13:19:08 -0500

    Update tbrandt_streamlit_linkedin_app.py

--- tbrandt_streamlit_linkedin_app.py
+++ tbrandt_streamlit_linkedin_app.py
@@ -81,7 +81,8 @@ def kpi_box(title, value):
         <p style="font-size: 24px; color: #555;">{value}</p>
     </div>
     """
-
+    
+user_input = pd.DataFrame()
 def main():
     st.set_page_config(page_title="LinkedIn User Prediction App", page_icon=":bar_chart:", layout="wide")
     st.markdown("<h1 style='color: #0a66c2;'>LinkedIn User Prediction App</h1>", unsafe_allow_html=True)

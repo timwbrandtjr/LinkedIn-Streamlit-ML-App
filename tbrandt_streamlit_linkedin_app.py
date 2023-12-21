829891d876aeaa64cdf1b1c5a31638fe958b7437
Timothy Brandt <153303831+timwbrandtjr@users.noreply.github.com>
2023-12-21 13:30:59 -0500

    Update tbrandt_streamlit_linkedin_app.py

--- tbrandt_streamlit_linkedin_app.py
+++ tbrandt_streamlit_linkedin_app.py
@@ -99,31 +99,37 @@ def main():
     # Sidebar with user input
     st.sidebar.header("User Input Features")
 
-    # Create a helper popup for income
+   # Create a helper popup for income
     st.sidebar.markdown("Income (Household):")
-    st.sidebar.info("""
-        1. Less than $10,000
-        2. $10,000 to under $20,000
-        3. $20,000 to under $30,000
-        4. $30,000 to under $40,000
-        5. $40,000 to under $50,000
-        6. $50,000 to under $75,000
-        7. $75,000 to under $100,000
-        8. $100,000 to under $150,000
-        9. $150,000 or more
+    st.sidebar.markdown("""
+        <details>
+            <summary>Details</summary>
+            <p>1. Less than $10,000</p>
+            <p>2. $10,000 to under $20,000</p>
+            <p>3. $20,000 to under $30,000</p>
+            <p>4. $30,000 to under $40,000</p>
+            <p>5. $40,000 to under $50,000</p>
+            <p>6. $50,000 to under $75,000</p>
+            <p>7. $75,000 to under $100,000</p>
+            <p>8. $100,000 to under $150,000</p>
+            <p>9. $150,000 or more</p>
+        </details>
     """)
 
     # Create a helper popup for education
     st.sidebar.markdown("Education:")
-    st.sidebar.info("""
-        1. Less than high school (Grades 1-8 or no formal schooling)
-        2. High school incomplete (Grades 9-11 or Grade 12 with NO diploma)
-        3. High school graduate (Grade 12 with diploma or GED certificate)
-        4. Some college, no degree (includes some community college)
-        5. Two-year associate degree from a college or university
-        6. Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)
-        7. Some postgraduate or professional schooling, no postgraduate degree (e.g., some graduate school)
-        8. Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)
+    st.sidebar.markdown("""
+        <details>
+            <summary>Details</summary>
+            <p>1. Less than high school (Grades 1-8 or no formal schooling)</p>
+            <p>2. High school incomplete (Grades 9-11 or Grade 12 with NO diploma)</p>
+            <p>3. High school graduate (Grade 12 with diploma or GED certificate)</p>
+            <p>4. Some college, no degree (includes some community college)</p>
+            <p>5. Two-year associate degree from a college or university</p>
+            <p>6. Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)</p>
+            <p>7. Some postgraduate or professional schooling, no postgraduate degree (e.g., some graduate school)</p>
+            <p>8. Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)</p>
+        </details>
     """)
 
     income = st.sidebar.slider("Income", 1, 9, 5, key="income_slider")

02cb73a4983bf766995735784813d8fb725beb80
Timothy Brandt <153303831+timwbrandtjr@users.noreply.github.com>
2023-12-22 10:41:15 -0500

    Update tbrandt_streamlit_linkedin_app.py

--- tbrandt_streamlit_linkedin_app.py
+++ tbrandt_streamlit_linkedin_app.py
@@ -127,7 +127,7 @@ def main():
 
     # Create a dropdown for education
     education = st.sidebar.selectbox("Education 🎓 (Highest Level)", list(education_options.keys()), format_func=lambda x: education_options[x])
-    st.write(f"Selected Education: {education}")
+    # st.write(f"Selected Education: {education}")
 
     parent = st.sidebar.radio("Parent 👨‍👩‍👦", ["No", "Yes"], key="parent_radio", help="Parental Status", )
     marital_status = st.sidebar.radio("Marital Status ❤️", ["Single", "Married"], key="marital_status_radio")
